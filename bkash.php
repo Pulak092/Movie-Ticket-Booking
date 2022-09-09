2490065e1ce38261720502bc8b9016bdf2cd0898
Pulak092 <87315723+Pulak092@users.noreply.github.com>
2022-09-09 20:31:16 +0600

    Add files via upload

--- bkash.php
+++ bkash.php
@@ -1,17 +1,3 @@
-<?php
-error_reporting(0);
-
-$mysqli = new mysqli ('localhost', 'root', '', 'theater_db');
-
-if(isset($_POST['Enter']))
-{
-    echo '<script type ="text/JavaScript">';  
-    echo 'alert("Payment Successfully")';  
-    echo '</script>';
-    header("Refresh:0; url=receipt.php");
-}
-?>
-
 <!DOCTYPE html>
 
 <!-- === Coding by CodingLab | www.codinglabweb.com === -->
@@ -21,36 +7,17 @@ if(isset($_POST['Enter']))
       <title>Bkash Payment</title>
    </head>
    <style>
-    body{
-        background-color: #FF1493;
-    }
-     .box{
-        position: absolute;
-        margin: auto;
-        top: 0;
-        right: 0;
-        bottom: 0;
-        left: 0;
-        width: 400px;
-        height: 380px;
-        box-shadow: 0px 5px 10px 0px rgba(0, 0, 0, 0.5)
-
-     }
-
    </style>
 
 <body>
-    <div class="box">
-        <div style="margin-left: 10%;">
-        <h1>Bkash Payment:</h1><br>
-            <form action="" method="POST">
-                <h4>Mobile Number:</h4>
-                <input type="number" required style="width: 86%; height: 30px;">
-                <h4>Password:</h4>
-                <input type="password" required style="width: 86%; height: 30px; ">
-                <br><br><br>
-                <input type="submit" name="Enter" value="Enter" style="width: 86%; height: 30px;background-color: #FF1493; " >
-            </form>
+    <div class="box" style="margin-top: 10%;">
+        <div >
+        <form action="">
+            <h4>Mobile Number:</h4>
+            <input type="number">
+            <h4>Password:</h4>
+            <input type="password">
+        </form>
         </div>
         
 
