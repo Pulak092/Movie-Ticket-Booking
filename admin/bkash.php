1cbc1ceb4e8a66d546b846fb7d0dac1e88905f42
Pulak092 <87315723+Pulak092@users.noreply.github.com>
2022-09-10 10:20:02 +0600

    Add files via upload

---
+++ admin/bkash.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<!-- === Coding by CodingLab | www.codinglabweb.com === -->
+<html lang="en" dir="ltr">
+   <head>
+      <meta charset="utf-8">
+      <title>Bkash Payment</title>
+   </head>
+   <style>
+   </style>
+
+<body>
+    <div class="box" style="margin-top: 10%;">
+        <div >
+        <form action="">
+            <h4>Mobile Number:</h4>
+            <input type="number">
+            <h4>Password:</h4>
+            <input type="password">
+        </form>
+        </div>
+        
+
+    </div>
+</body>
+
+</html>
